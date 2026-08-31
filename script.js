3d3c2c7339f3ed9e11d36ef0f1731b614434f2b1
cenacchi2000 <filo.cenacchi@gmail.com>
2026-08-31 14:14:03 +1000

    Add mobile menu toggle functionality

---
+++ script.js
@@ -0,0 +1,5 @@
+const menuButton = document.querySelector('.menu-button');
+const navLinks = document.querySelector('.nav-links');
+menuButton?.addEventListener('click', () => {
+  navLinks.classList.toggle('mobile-open');
+});
